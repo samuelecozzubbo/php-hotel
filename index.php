2c74d527e6adb4f4b6e80e7480f844e396e618b9
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-07-24 15:29:03 +0200

    Feat: import table from bootstrap and add t-head with hotels-keys

--- index.php
+++ index.php
@@ -1,3 +1,50 @@
+<?php
+/* Array di hotels */
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+    $hotel_keys= array_keys($hotels[0]);
+    var_dump($hotel_keys);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +55,41 @@
     <title>php-hotel</title>
 </head>
 <body>
+    <div class="container my-5">
+        <h1>PHP HOTEL</h1>
+        <div class="row">
+            <div class="col-12">
+            <table class="table">
+  <thead>
+    <tr>
+        <?php foreach($hotel_keys as $key): ?>
+            <?php echo "<th scope='col'>$key</th>"?>
+        <?php endforeach ?>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+            </div>
+        </div>
+    </div>
     
 </body>
 </html>
